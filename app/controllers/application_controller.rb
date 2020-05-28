cc457d241fb2c999664a2761c2a9b1d1b9b2e3f6
Winifred Frierson <winifred.dykes@hotmail.com>
2020-05-28 07:38:11 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,43 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  # code actions here!
+  get '/recipes' do
+    @recipes = Recipe.all
+    erb :index
+  end
+  
+  get '/recipes/new' do
+    erb :new
+  end
+  
+  post "/recipes" do
+    @recipes = Recipe.create(params)
+    redirect to "/recipes/#{@recipes.id}"
+  end
+  
+  get 'recipes/:id' do
+    @recipes = Recipe.find_by_id(params[:id])
+    erb :edit
+  end
+  
+  get '/recipes/:id/edit' do
+    @recipes = Recipe.find_by_id(params[:id])
+    erb :show
+  end
+  
+  patch '/recipes/:id' do
+    @recipes = Recipe.find_by_id(params[:id])
+    @recipes.name = params[:name]
+    @recipes.ingredients = params[:ingredients]
+    @recipes.cook_time = params[:cook_time]
+    @recipes.save
+    redirect to "/recipes/#{recipes.id}"
+  end
+  
+  delete '/recipes/:id' do
+    @recipes = Recipe.find_by_id(params[:id])
+    @recipes.delete
+    redirect to "/recipes"
+  end
 
 end
